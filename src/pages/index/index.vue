4a25749dcb5a53dafb344412445e2edafaa0e690
zhenghanreal <2100600378@qq.com>
2024-04-03 18:02:42 +0800

    自动引入自定义组件，轮播图下标指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,10 @@ import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 引入自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 引入轮播图 -->
+  <XtxSwiper></XtxSwiper>
   <view class="index">index</view>
 </template>
 
