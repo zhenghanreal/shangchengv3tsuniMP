0e7e5ae8032889579d387f6dc03a40b1c0b44dbb
zhenghanreal <2100600378@qq.com>
2024-04-06 19:40:17 +0800

    商品详情页面渲染

--- src/services/category.ts
+++ src/services/category.ts
@@ -1,6 +1,7 @@
 import type { CategoryTopItem } from '@/types/categroy'
 import { http } from '@/utils/http'
 
+//商品分类接口
 export const gatCategoryTop = () => {
   return http<CategoryTopItem[]>({
     method: 'GET',
